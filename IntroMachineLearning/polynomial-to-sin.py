3b175ca88f31212b562acb86c69ca3478117d46e
Tamara Rico <tamara.rico@uabc.edu.mx>
2021-12-27 14:05:46 -0800

    Update and rename polinomial-to-sin.py to polynomial-to-sin.py

--- IntroMachineLearning/polynomial-to-sin.py
+++ IntroMachineLearning/polynomial-to-sin.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
+# Código a estudiar obtenido de :
+# https://pytorch.org/tutorials/beginner/examples_autograd/polynomial_autograd.html#sphx-glr-beginner-examples-autograd-polynomial-autograd-py
+# MODIFICADO
 
-#polinomio de grado 3 que se aproxime al seno de pi
+# polinomio de grado 3 que se aproxime al seno de pi
 # es en un sistema fundamental lo que hace un red neuronal
-#porque esta tratando de aproximarse a un resultado
+# porque esta tratando de aproximarse a un resultado
 
 
 import torch
@@ -16,13 +19,12 @@ device = torch.device("cpu")
 # Create Tensors to hold input and outputs.
 # By default, requires_grad=False, which indicates that we do not need to
 # compute gradients with respect to these Tensors during the backward pass.
-#tienes una funcion y 2000 puntos que se pueden aproximar
+# tienes una funcion y 2000 puntos que se pueden aproximar
 x = torch.linspace(-math.pi, math.pi, 2000, device=device, dtype=dtype)
 
 y = torch.sin(x)
-
-#Tensors : a, b, c, d
-#numbers put together
+# Tensors : a, b, c, d
+# A Tensor represents a node in a computational graph. 
 
 # Create random Tensors for weights. For a third order polynomial, we need
 # 4 weights: y = a + b x + c x^2 + d x^3
@@ -33,51 +35,48 @@ b = torch.randn((), device=device, dtype=dtype, requires_grad=True)
 c = torch.randn((), device=device, dtype=dtype, requires_grad=True)
 d = torch.randn((), device=device, dtype=dtype, requires_grad=True)
 
-#Y= a + bx + cx^2 + dx3
-#se inicializa cada valor del polinomios en valores rand
+# Y = a + bx + cx^2 + dx3
+# se inicializa cada valor del polinomios en valores random
 
 
 learning_rate = 1e-6
-for t in range(2000):#for loop para pasar por todos los puntos
-    # Forward pass: compute predicted y using operations on Tensors.
+for t in range(2000):# for loop para pasar por todos los puntos
+    # Forward pass: compute predicted "y" using operations on Tensors.
     y_pred = a + b * x + c * x ** 2 + d * x ** 3
-#funcion que maneja el programa
+# funcion que maneja el programa
 
     # Compute and print loss using operations on Tensors.
     # Now loss is a Tensor of shape (1,)
     # loss.item() gets the scalar value held in the loss.
     loss = (y_pred - y).pow(2).sum()
-    #loss representa la diferencua entre el valor que estamos obteniendo y el valor que debe ser
+    # loss representa la diferencia entre el valor que estamos obteniendo y el valor que debemos obtener
     if t % 100 == 99:
         print(t, loss.item())
-    #si el numero es cercano lo imprime
+    # si el numero es cercano lo imprime
 
-    #calculamos el loss para que nosotros sepamos como arreglar ese problrema
-    #
+    # calculamos e imprimimos loss para saber cómo el programa esta arreglando ese problrema
 
     # Use autograd to compute the backward pass. This call will compute the
     # gradient of loss with respect to all Tensors with requires_grad=True.
     # After this call a.grad, b.grad. c.grad and d.grad will be Tensors holding
     # the gradient of the loss with respect to a, b, c, d respectively.
-    loss.backward()
-    #esta funcion es la forma en la que la maquinna esta viendo como estan saliendo los resultado
-    #y como deberian ajustarlos
-
-    #gradient es una forma abstracta de decirnos la pendiente, que tanto nos alejamos o acercamos
+    loss.backward() # backpropagation / retroalimentacion
+    # esta funcion es la forma en la que la maquina esta viendo como estan saliendo los resultado y como deberian ajustarlos
+    # gradient es una forma abstracta de decirnos la pendiente, que tanto nos alejamos o acercamos
 
     # Manually update weights using gradient descent. Wrap in torch.no_grad()
     # because weights have requires_grad=True, but we don't need to track this
     # in autograd.
     
-    #aqui se estan usando esas slopes (pendientes) para ajustar el resultado
+    # aqui se estan usando esas slopes (pendientes) para ajustar el resultado
     with torch.no_grad():
-        a -= learning_rate * a.grad #al valor de a se le resta el learning rate que se multiplica por el slope
-        b -= learning_rate * b.grad #a.grad nos dice la pendiente del punto respecto a la parabola
-        c -= learning_rate * c.grad # usamos en menos porque nos podemos mover a ambos lados de la parabola, izq o derecha
-        d -= learning_rate * d.grad #el learning rate es que tanto queremos que cambie el valor de las variables abcd
+        a -= learning_rate * a.grad # al valor de "a" se le resta el learning rate que se multiplica por el slope
+        b -= learning_rate * b.grad # a.grad nos dice la pendiente del punto respecto a la parabola
+        c -= learning_rate * c.grad # usamos el menos porque nos podemos mover a ambos lados de la parabola, izq o derecha
+        d -= learning_rate * d.grad # el learning rate es que tanto queremos que cambie el valor de las variables abcd
 
         # Manually zero the gradients after updating weights
-        a.grad = None #reset the gradients para la siguiente iteracion
+        a.grad = None # reset the gradient for next iteration
         b.grad = None
         c.grad = None
         d.grad = None
